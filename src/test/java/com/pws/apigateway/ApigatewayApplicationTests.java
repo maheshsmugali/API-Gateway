bf95aacc9d7eacd56ff38768f4da53c70b794033
maheshsmugali <maheshsmugali@gmail.com>
2023-03-31 18:31:38 +0530

    commit

---
+++ src/test/java/com/pws/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pws.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
